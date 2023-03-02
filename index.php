3534aa25d7a413f99633f73ce44301a138ab4afb
Offlin-e <120373341+Offlin-e@users.noreply.github.com>
2023-03-02 16:20:56 +0100

    ajout style.css

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>AIR ONLINE - Paper Wings Championship</title>
-  <link type="text/css" rel="stylesheet" href="style.css">
-  <link rel="shortcut icon" href="" type="image/x-icon">
-</head>
-<body>
-<section>compte à rebourd</section>
-<section>prez disciplines</section>
-<section>bouton inscription</section>
-<section>infos directions</section>
-<section>vignettes</section>
-<section>partenaires</section>
-<section>form</section>
-<footer>footer</footer>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>AIR ONLINE - Paper Wings Championship</title>
+        <link type="text/css" rel="stylesheet" href="style.css">
+        <link rel="shortcut icon" href="" type="image/x-icon">
+    </head>
+    <body>
+        <section>
+            <div>
+                <img src="assets/logos_aironline/logo2_fond_noir.svg" alt="logo Aironline">
+                <img src="assets/avions&icones/facebook.svg" alt="logo Facebook">
+                <img src="assets/avions&icones/instagram.svg" alt="logo Instagram">
+            </div>
+            <div>
+                <h1>PAPER WINGS CHAMPIONSHIP</h1>
+            </div>
+        </section>
+        <section>prez disciplines</section>
+        <section>bouton inscription</section>
+        <section>infos directions</section>
+        <section>vignettes</section>
+        <section>partenaires</section>
+        <section>form</section>
+        <footer>footer</footer>
+    </body>
 </HTML>
