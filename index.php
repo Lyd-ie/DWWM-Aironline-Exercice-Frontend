811ba4563b79c795724e78044c9e9089cfad64a8
Offlin-e <120373341+Offlin-e@users.noreply.github.com>
2023-03-02 15:35:07 +0100

    ajout assets + sections de base

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-coucou
-salut
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>AIR ONLINE - Paper Wings Championship</title>
+  <link type="text/css" rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="" type="image/x-icon">
+</head>
+<body>
+<section>compte à rebourd</section>
+<section>prez disciplines</section>
+<section>bouton inscription</section>
+<section>infos directions</section>
+<section>vignettes</section>
+<section>partenaires</section>
+<section>form</section>
+<footer>footer</footer>
+</body>
+</HTML>
