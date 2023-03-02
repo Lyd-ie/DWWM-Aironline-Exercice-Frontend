18bd3c86d127f88ec3d9fbd5ba5cba40008d9d9a
Montoute/Arayther <montoute.jose@gmail.com>
2023-03-02 16:22:07 +0100

    création du formulaire

--- index.php
+++ index.php
@@ -13,7 +13,26 @@
 <section>infos directions</section>
 <section>vignettes</section>
 <section>partenaires</section>
-<section>form</section>
+<section>
+  <div>
+    <form action="POST" action="index.php">
+
+      <div>
+        <input type="text" name="nom" placeholder="NOM du PARTICIPANT.......................................................................................................">
+      </div>
+
+      <div>
+        <input type="text" name="prenom" placeholder="PRENOM............................................................................................ "><input type="text" name="age" placeholder="AGE........................">
+      </div>
+
+      <div>
+        <input type="text" name="email" placeholder="Adresse mail..................................................................................@...............................">
+      </div>
+
+
+    </form>
+  </div>
+</section>
 <footer>footer</footer>
 </body>
 </HTML>
