8e9acc6687e80481f722a4c3e431c2b8c79b985c
Montoute/Arayther <montoute.jose@gmail.com>
2023-03-03 09:40:50 +0100

    liaison a la base de donnée

--- index.php
+++ index.php
@@ -1,3 +1,55 @@
+<?php 
+// Configuration de la connexion
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','root');
+define('DB_NAME','aironline');
+
+try
+{
+    // Connexion � la base
+    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
+}
+catch (PDOException $e)
+{
+	// Echec de la connexion
+    exit("Error: " . $e->getMessage());
+}
+
+if(isset($_POST['submit'])) {
+
+  // On récupère le nom saisi par le lecteur
+  $name = $_POST['nom'];
+
+  // On récupère le prenom du lecteur
+  $firstName = $_POST['prenom'];
+
+  // On récupère l'age du lecteur
+  $age = $_POST['age'];
+
+  // On récupère l'email
+  $email = $_POST['email'];
+
+// On fixe le statut du lecteur à 1 par défaut (actif)
+$status = 1;
+
+// On prépare la requête d'insertion en base de données de toutes ces valeurs dans la table tblreaders
+$query = $dbh->prepare("INSERT INTO inscription(LastName, FirstName, Age, Email, Status) VALUES (:LastName, :FirstName, :Age, :Email, :Status)");
+
+// On bind les paramètres
+$query->bindParam(':LastName', $name, PDO::PARAM_STR);
+$query->bindParam(':FirstName', $FirstName, PDO::PARAM_STR );
+$query->bindParam(':Age', $age, PDO::PARAM_INT);
+$query->bindParam(':Email', $email, PDO::PARAM_STR);
+$query->bindParam(':Status', $status, PDO::PARAM_INT);
+
+// On éxecute la requête
+$query->execute();
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -37,6 +89,8 @@
                 <div>
                     <input class="email" type="text" name="email" placeholder="Adresse mail..................................................................................@...............................">
                 </div>
+
+                <button  type="submit" name="submit" >Valider</button>
                 
             </form>
             </div>
